15256a51364cdfc85f137f1a48c43be47c733ea3
Parshvavyas17 <parshvavyas07@gmail.com>
2022-05-21 20:04:20 +0530

    Submit Functionality added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //import React from "react"; // Only when using classes
-import { useState } from "react"
+import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
@@ -28,20 +28,27 @@ function App() {
     ]
 )
 
+// Add a task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random()*1000)+1;
+    const newTask = { ...task,id }
+    setTasks([...tasks, newTask])
+  }
+
 // Delete a task
   const deleteTask = (id) => {
-    setTasks(tasks.filter((task)=> task.id !== id))
+    setTasks(tasks.filter((task)=> task.id !== id));
   }
 
 // Toggle reminder
   const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? {...task,reminder:!task.reminder} : task))
+    setTasks(tasks.map((task) => task.id === id ? {...task,reminder:!task.reminder} : task));
   }
 
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />):('No Tasks to show')}
     </div>
   );
