8462700949b5a00281a5ca776ecfc68d3b88b2a9
Parshvavyas17 <parshvavyas07@gmail.com>
2022-06-14 19:49:49 +0530

    Routing done added about, footer and task details

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 import { useEffect } from 'react';
+import Footer from "./components/Footer";
+import About from "./components/About";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TaskDetails from "./components/TaskDetails";
 
 function App() {
   const [showAddTask,setShowAddTask] = useState(false);
@@ -84,11 +88,22 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask) } showAdd={showAddTask} />
-      { showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />):('No Tasks to show')}
-    </div>
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask) } showAdd={showAddTask} />
+        <Routes>
+          <Route path="/" element={
+            <>
+            { showAddTask && <AddTask onAdd={addTask} />}
+            {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />):('No Tasks to show')}
+          </>
+          } />
+          <Route path="/about" element={<About />} />
+          <Route path="/task/:id" element={<TaskDetails />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Button from './Button'
+import { useLocation } from 'react-router-dom'
 
 const Header = (props) => {
+  const location = useLocation()
+
   return (
     <header className='header'>
       <h1>{props.title}</h1>
-      <Button text={props.showAdd ? 'Close' : 'Add' } color={props.showAdd ? 'red' : 'green' } onClick={props.onAdd}/>
+      {location.pathname === '/' && <Button text={props.showAdd ? 'Close' : 'Add' } color={props.showAdd ? 'red' : 'green' } onClick={props.onAdd}/>}
       {/* <Button title='Remove' color='red' onClick={on_click}/>
       <Button title='Edit' color='blue' onClick={on_click}/> */}
     </header>
