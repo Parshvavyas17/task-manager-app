d94e1b6e430d30a63a40e92fb78e0ecd995175f5
Parshvavyas17 <parshvavyas07@gmail.com>
2022-06-14 20:01:14 +0530

    Final changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,15 @@
-import { Link } from 'react-router-dom'
+import Button from './Button'
+import { useNavigate } from 'react-router-dom'
 
 const About = () => {
+  const navigate = useNavigate()
+
   return (
     <div>
         <h4>Version 1.0.0</h4>
-        <Link to='/'>Go back</Link>
+        <Button onClick={() => {
+          navigate(-1)
+          }} text='Go Back' />
     </div>
   )
 }
